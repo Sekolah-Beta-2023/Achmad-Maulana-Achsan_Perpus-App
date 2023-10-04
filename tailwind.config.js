0ac14fd67431e63e951e5e9b2587eba500395a1f
Achsmboh <maulana.achsanxiipa1@gmail.com>
2023-10-04 15:18:58 +0700

    installation font & color

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'green-light': '#CCEDF2',
+        'green-dark': '#00899D',
+        'orange-light': '#D6ABA1',
+        'black-light': '#4C4C4C',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        roboto: ['Roboto Slab', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
