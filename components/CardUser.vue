3f05ab98c1bcc5b562d2cac9d8b323a5bb91664b
Achsmboh <maulana.achsanxiipa1@gmail.com>
2023-10-06 14:38:31 +0700

    added component card user

---
+++ components/CardUser.vue
@@ -0,0 +1,39 @@
+<template>
+  <tr class="w-full border-green-dark border-solid border-2 h-12">
+    <td class="w-12 border-green-dark border-2 text-center">{{ no }}</td>
+    <td class="border-green-dark border-2 px-3">{{ user.username }}</td>
+    <td class="border-green-dark border-2 px-3">{{ user.email }}</td>
+    <td class="border-green-dark border-2 text-center">
+      {{ user.status ? 'meminjam' : 'tidak meminjam' }}
+    </td>
+    <td class="border-green-dark border-2 text-center">
+      <button class="w-7 h-7 rounded border-green-dark border-2">
+        <img src="../assets/icon-edit.svg" alt="icon-edit" />
+      </button>
+      <button class="w-7 h-7 rounded border-red-600 border-2">
+        <img src="../assets/icon-delete.svg" alt="icon-delete" />
+      </button>
+    </td>
+  </tr>
+</template>
+<script>
+export default {
+  name: 'CardUser',
+  props: {
+    user: {
+      type: Object,
+      default: () => {
+        return {
+          username: 'Username',
+          email: 'Email',
+          status: false,
+        }
+      },
+    },
+    no: {
+      type: Number,
+      default: 1,
+    },
+  },
+}
+</script>
