f7a12acadebe38fe3a9b29ad1a2ce45102c8336e
Achsmboh <maulana.achsanxiipa1@gmail.com>
2023-10-06 11:12:14 +0700

    add card detail

--- components/CardBook.vue
+++ components/CardBook.vue
@@ -3,7 +3,11 @@
     class="bg-white shadow-md rounded-2xl w-60 h-96 flex flex-col items-center p-3"
   >
     <div class="w-52 h-72 mb-1">
-      <img class="rounded-xl object-cover w-full h-72" :src="image" alt="" />
+      <img
+        class="rounded-xl object-cover w-full h-72"
+        :src="image"
+        :alt="image"
+      />
     </div>
     <div class="w-full h-full flex justify-center items-center">
       <h1 class="text-base font-inter text-green-dark text-center">
---
+++ components/CardDetail.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="bg-white p-5 shadow-lg rounded-lg">
+    <div class="flex gap-7">
+      <CardBook :image="buku.urlImage" />
+      <div
+        class="text-green-dark text-base font-inter flex flex-col justify-between"
+      >
+        <div class="">
+          <tr>
+            <td class="font-bold">Judul</td>
+            <td class="font-bold block mr-3">:</td>
+            <td class="ml-2">{{ buku.judul }}</td>
+          </tr>
+          <tr>
+            <td class="font-bold">Penulis</td>
+            <td class="font-bold block mr-3">:</td>
+            <td class="ml-2">{{ buku.penulis }}</td>
+          </tr>
+          <tr>
+            <td class="font-bold">Penerbit</td>
+            <td class="font-bold block mr-3">:</td>
+            <td class="ml-2">{{ buku.penerbit }}</td>
+          </tr>
+          <tr>
+            <td class="font-bold">Tahun</td>
+            <td class="font-bold block mr-3">:</td>
+            <td class="ml-2">{{ buku.tahun }}</td>
+          </tr>
+          <tr>
+            <td class="font-bold">ISBN</td>
+            <td class="font-bold block mr-3">:</td>
+            <td class="ml-2">{{ buku.isbn }}</td>
+          </tr>
+          <tr>
+            <td class="font-bold">Deskripsi</td>
+            <td class="font-bold block mr-3">:</td>
+            <td class="ml-2">
+              {{ buku.deskripsi }}
+            </td>
+          </tr>
+        </div>
+        <div class="flex w-full justify-end my-4 gap-5">
+          <button
+            class="bg-red-500 w-48 text-base font-bold h-12 shadow-lg rounded-md text-white"
+          >
+            Hapus
+          </button>
+          <button
+            type="submit"
+            class="bg-green-dark w-48 text-base font-bold h-12 shadow-lg rounded-md text-white"
+          >
+            Edit
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import CardBook from './CardBook.vue'
+
+export default {
+  name: 'CardDetail',
+  components: { CardBook },
+  props: {
+    buku: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
